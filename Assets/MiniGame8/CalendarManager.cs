63a1637465456e350b184b99e8db9b70a131a313
Puddin26 <jl12476@nyu.edu>
2024-11-06 23:06:51 -0500

    minor fixes for game 8

--- Assets/MiniGame8/CalendarManager.cs
+++ Assets/MiniGame8/CalendarManager.cs
@@ -1,36 +1,16 @@
 using UnityEngine;
-using TMPro; 
-using System.Collections.Generic;
 
-public class CalendarManager : MonoBehaviour
+public class CalendarManager : SnapManager
 {
-    public int totalObjects = 4;
-    public int snappedCount;
-    public TextMeshProUGUI arcanaText;
-    private HashSet<SimplifiedObjectMoveAndSnap> snappedObjects = new HashSet<SimplifiedObjectMoveAndSnap>();
-
-    void OnEnable()
+    protected override void HandleObjectSnapped(SimplifiedObjectMoveAndSnap snappedObject)
     {
-        SimplifiedObjectMoveAndSnap.OnObjectSnapped += HandleObjectSnapped;
-    }
+        base.HandleObjectSnapped(snappedObject);
 
-    void OnDisable()
-    {
-        SimplifiedObjectMoveAndSnap.OnObjectSnapped -= HandleObjectSnapped;
-    }
-
-    void HandleObjectSnapped(SimplifiedObjectMoveAndSnap snappedObject)
-    {
-        if (!snappedObjects.Contains(snappedObject))
-        {
-            snappedObjects.Add(snappedObject);
-            snappedCount++;
-            arcanaText.text = $"Major Arcana ({snappedCount}/22)";
-        }
-
+        // Check if all objects are snapped in place
         if (snappedObjects.Count == totalObjects)
         {
-            // Actions when all objects are snapped, if needed
+            // Things after all objects snapped in place
+            Debug.Log("Finished");
         }
     }
 }
